7b91df6ed6b4830cb51996de2bdf2ddc2d341043
Brent <brent.clark@xneelo.com>
2023-12-22 14:11:36 +0200

    GET / POST posts

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
-from typing import Optional
+from typing import Optional, List
 from fastapi import FastAPI, Response, status, HTTPException
-from fastapi.params import Body
-from pydantic import BaseModel
-from random import randrange
 from datetime import datetime
 
 from sqlmodel import Field, Session, SQLModel, create_engine, select
@@ -11,8 +8,8 @@ class Posts(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str
     content: str
-    published: bool
-    created_at: datetime = datetime.now
+    published: bool = Field(default=True, nullable=False)
+    created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
 
 sqlite_file_name = "posts.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -22,8 +19,34 @@ engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
 
 app = FastAPI()
 
-@app.get("/posts/")
-def read_posts():
+@app.get("/")
+def root():
+    return {"message": "Check /redoc or /docs"}
+
+# Get - All posts
+@app.get("/posts/", response_model=List[Posts])
+def read_all_posts():
     with Session(engine) as session:
         posts = session.exec(select(Posts)).all()
         return posts
+
+# Get - A post
+@app.get("/posts/{post_id}")
+def read_a_post(post_id: int):
+    with Session(engine) as session:
+        statement = select(Posts).where(Posts.id == post_id)
+        result = session.exec(statement)
+        post = result.first() 
+        return {
+            "id": post
+        }
+
+# Post
+@app.post("/posts/", response_model=Posts)
+def create_post(post: Posts):
+    print(post)
+    with Session(engine) as session:
+        session.add(post)
+        session.commit()
+        session.refresh(post)
+        return post
