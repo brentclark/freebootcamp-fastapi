b2a6e36f2cc5fb4bd273e4bb449b914a85131871
Brent <brent.clark@xneelo.com>
2023-12-22 12:36:23 +0200

    Updated README, schema and Fastapi GET code

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ python3 -m pip install -r requirements.txt
 python3 -m pip freeze > requirements.txt
 
 sqlite3 create_database.db < create_database.sql
+
+uvicorn app.main:app --reload
--- app/main.py
+++ app/main.py
@@ -3,5 +3,27 @@ from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
+from datetime import datetime
+
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+class Posts(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    content: str
+    published: bool
+    created_at: datetime = datetime.now
+
+sqlite_file_name = "posts.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
 
 app = FastAPI()
+
+@app.get("/posts/")
+def read_posts():
+    with Session(engine) as session:
+        posts = session.exec(select(Posts)).all()
+        return posts
