ac8e6ef61a57c651a7ef433942081e0b9cdffb21
Brent <brent.clark@xneelo.com>
2023-12-27 16:13:42 +0200

    Added Oauth2 autentication

--- app/schemas.py
+++ app/schemas.py
@@ -54,8 +54,15 @@ class UserResponse(BaseModel):
     class Config:
         orm_mode = True
 
-class User(UserResponse):
+        
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
 
-    class Config:
-        orm_mode = True
+    
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
+class TokenData(BaseModel):
+    id: Optional[str] = None
