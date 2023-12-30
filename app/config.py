68c82d3ecfb954e3bd79faa814eb92b0bc78bf49
Brent <brent.clark@xneelo.com>
2023-12-30 22:04:51 +0200

    Changed to config

---
+++ app/config.py
@@ -0,0 +1,15 @@
+from typing import Any
+from pydantic_settings import BaseSettings
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv(".env"))
+
+
+class Settings(BaseSettings):
+    SQLALCHEMY_DATABASE_URL: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int 
+    SECRET_KEY: str
+    ALGORITHM: str
+
+settings: Any = Settings()
+# config: str = Config(".env")
