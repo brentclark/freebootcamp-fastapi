060b3de7468f12b1018ae375811795d5ef9b6036
Brent <brent.clark@xneelo.com>
2024-01-05 11:35:55 +0200

    Added print

--- app/database.py
+++ app/database.py
@@ -7,6 +7,8 @@ engine = create_engine(
     settings.SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
+print(f"mysql+pymysql://{settings.MYSQL_USER}:{settings.MYSQL_PASSWORD}@{settings.MYSQL_HOST}/{settings.MYSQL_DATABASE}?charset=utf8mb4")
+
 engine = create_engine(
     "mysql+pymysql://{settings.MYSQL_USER}:{settings.MYSQL_PASSWORD}@{settings.MYSQL_HOST}/{settings.MYSQL_DATABASE}?charset=utf8mb4"
 )
