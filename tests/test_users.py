049aaf287be7b62770a089a32aedfe9b7672ec17
Brent <brent.clark@xneelo.com>
2024-01-16 12:35:53 +0200

    Added Faker module, and test test_create_user()

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,46 +3,43 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, engine
 from sqlalchemy.orm import sessionmaker
-import faker 
+from faker import Faker
 
 from app.oauth2 import create_access_token
 from app.config import settings
 from app.database import engine, Base
-
-# from app.db.base import Base
-# from app.db.session import SessionLocal
+from app.schemas import UserResponse
 from app.main import app
+from datetime import datetime
 
-# engine = create_engine(  # noqa
-#    f"mysql+pymysql://{settings.MYSQL_USER}:{settings.MYSQL_PASSWORD}@{settings.MYSQL_HOST}/{settings.MYSQL_DATABASE}?charset=utf8mb4",
-# )
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(bind=engine)
 
 client = TestClient(app)
 
 
-# @pytest.fixture(scope="function")
-# def db():
-#    yield SessionLocal()
-#
-#
-# @pytest.fixture(scope="function")
-# def client(db) -> Generator:
-#    try:
-#        TestClient(app)
-#        yield TestClient(app)
-#    finally:
-#        db.close()
+def is_valid_email(email):
+    import re
+
+    pattern = r"^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$"
+    return re.match(pattern, email)
+
+
 @pytest.mark.parametrize(
-    ("email", "password"), (faker.company_email(), faker.password(length=25))
+    ("email", "password", "status"),
+    ((Faker().company_email(), Faker().password(length=25), 201),),
 )
-def test_create_user():
+def test_create_user(email, password, status):
     response = client.post(
         "/users",
         json={"email": email, "password": password},
     )
-    assert response.status_code == 201
+    user_response = UserResponse(**response.json())
+
+    assert response.status_code == status
+    assert user_response.email == email
+    assert isinstance(user_response.id, int)
+    assert isinstance(user_response.created_at, datetime)
 
 
 def test_get_users():
@@ -57,8 +54,17 @@ def test_get_user_with_id_1():
     response = client.get(
         "/users/1",
     )
+    print(response.json())
+
     assert response.status_code == 200
     assert response.json().get("id") == 1
     assert response.json().get("email") is not None
     assert response.json().get("created_at") is not None
-    print(response.json())
+
+    user_response = UserResponse(**response.json())
+    assert isinstance(user_response.id, int)
+    assert isinstance(user_response.created_at, datetime)
+    assert (
+        is_valid_email(user_response.email) != None
+    ), f"Expected {user_response.email}, i.e. Does not match"
+
